34dbcb0d1cab27497601c4dcced82ad72cb6aab2
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-05-28 11:27:52 +0300

    Update Test.js

--- commandes/Test.js
+++ commandes/Test.js
@@ -4,9 +4,9 @@ const { france } = require("../framework/france");
 france({ nomCom: "test", reaction: "⏳", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '𝘽𝙊𝙏 𝙄𝙎 𝙊𝙉𝙇𝙄𝙉𝙀 \n\n ' + "║❒ 𝙏𝙔𝙋𝙀 𝙈𝙀𝙉𝙐 𝙏𝙊 𝙎𝙀𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎";
-    let d = '                                            ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@ibrahimaitech';
+    let d = '                                            ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/Rhodvick';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/16dd2b3823fedec5c8d88.jpg';
+    var img = 'https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -19,9 +19,9 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *BMW-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *BMW-MD-REPO*'
+   let z ='Salut je m\'appelle *MBUVI-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *MBUVI-MD-REPO*'
       let varmess=z+d
-      var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
+      var img='https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
