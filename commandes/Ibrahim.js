a4f6675ca6985ed1b11720eb7a1f5ac3e857712d
MBUVI DAVID <147725809+cheekydavy@users.noreply.github.com>
2024-05-28 11:16:53 +0300

    Update Ibrahim.js

--- commandes/Ibrahim.js
+++ commandes/Ibrahim.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "ibrahim", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "mbuvi", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
-    let d = '                                     ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@ibrahimaitech';
+    let z = 'Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D";
+    let d = '                                     ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@Rhodvick';
     let varmess = z + d;
-    var mp4 = 'https://telegra.ph/file/dec033e457813cd2ff2e3.mp4';
+    var mp4 = 'https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
     //console.log("montest")
 });
@@ -19,9 +19,9 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *BMW-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *BMW-MD-REPO*'
+   let z ='Salut je m\'appelle *MBUVI-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *MBUVI-MD-REPO*'
       let varmess=z+d
-      var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
+      var img='https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
